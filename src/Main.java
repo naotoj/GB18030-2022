b3f9d9b32eea1609892ba0cbdc1536482bd3569b
Naoto Sato <naoto.sato@oracle.com>
2023-01-31 18:11:47 -0800

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -11,38 +12,43 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.nio.file.Files.newBufferedReader;
+import static java.nio.file.Files.newBufferedWriter;
+
 public class Main {
     private static final Charset GB18030 = Charset.forName("GB18030");
+    private static final CharsetEncoder GB18030_ENC = GB18030.newEncoder();
     private static final HexFormat HF = HexFormat.of();
     private static final Path OUTFILE = Path.of("out/out");
 
     public static void main(String[] args) throws Exception {
-        var map = new HashMap<>(loadMap("src/MappingTableBMP.txt"));
-        map.putAll(loadMap("src/MappingTableSMP.txt"));
+        var encMap = new HashMap<>(loadMap("src/MappingTableBMP.txt", true));
+        encMap.putAll(loadMap("src/MappingTableSMP.txt", true));
 
-//        map.keySet().stream().sorted()
-//                .forEach(c -> check(map, c));
         var output = IntStream.range(0, 0x10FFFF)
-                .mapToObj(c -> check(map, c))
+                .mapToObj(c -> check(encMap, c))
                 .filter(Objects::nonNull)
                 .toList();
         Files.deleteIfExists(OUTFILE);
+        var bw = Files.newBufferedWriter(OUTFILE, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         for (String str : output) {
-//            System.out.println(str);
-            Files.writeString(OUTFILE, str + "\n", StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            bw.append(str);
+            bw.newLine();
         }
+        bw.close();
     }
 
-    static Map<Integer, Integer> loadMap(String mapfile) throws Exception {
+    static Map<Integer, Integer> loadMap(String mapfile, boolean isEncoding) throws Exception {
         return Files.readAllLines(Path.of(mapfile)).stream()
                 .map(line -> line.split("[\s\t]+"))
-                .collect(Collectors.toMap(m -> Integer.valueOf(m[0], 16), m -> Integer.parseUnsignedInt(m[1], 16)));
+                .collect(Collectors.toMap(m -> Integer.parseUnsignedInt(m[isEncoding ? 0 : 1], 16),
+                        m -> Integer.parseUnsignedInt(m[isEncoding ? 1 : 0], 16)));
     }
 
     static String check(Map<Integer, Integer> mapping, Integer c) {
-        var bytes = HF.formatHex(Character.toString(c).getBytes(GB18030));
+        var bytes = HF.formatHex(Character.toString(c).getBytes(GB18030)).replaceFirst("^0+", "");
         var mapped = Integer.toUnsignedString(mapping.getOrDefault(c, 0x3f), 16);
-        return bytes.equals(mapped) ?
+        return  mapped.equals("0") || mapped.equals("3f") || bytes.equals(mapped) ?
                 null :
                 "cp: " + Integer.toHexString(c) + ", map: " + mapped + ", conv: " + bytes;
     }
